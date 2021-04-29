a719e60684f4b315a97f80c8bd6b52e29f6ce2c7
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-04-29 17:00:36 -0400

    changes to hsndlebars

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,13 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
+    if (username && password) {
+      const response = await fetch('/api/user', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
